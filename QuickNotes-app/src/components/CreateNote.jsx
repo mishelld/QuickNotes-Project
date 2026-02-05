15d644a64947525f7e82aa044de7f323a30069e8
mishelld <mishelldub@gmail.com>
2026-02-05 11:04:19 +0200

    feat: add components for creating and displaying notes with basic styling.

--- QuickNotes-app/src/components/CreateNote.jsx
+++ QuickNotes-app/src/components/CreateNote.jsx
@@ -4,8 +4,29 @@ import Notes from "./Notes";
 function CreateNote() {
   const [value, setValue] = useState("");
   const [notes, setNotes] = useState([]);
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const getDate = () => {
+    const month = monthNames[new Date().getMonth()];
+    const day = new Date().getDate().toLocaleString();
+    const hour = new Date().getHours().toLocaleString().padStart(2, "0");
+    const minutes = new Date().getMinutes().toLocaleString().padStart(2, "0");
+    return `${month} ${day}st ${hour}:${minutes}`;
+  };
   const handleAdd = () => {
-    setNotes([...notes, value]);
+    setNotes([...notes, { text: value, date: getDate() }]);
     setValue("");
   };
 
--- QuickNotes-app/src/components/Notes.jsx
+++ QuickNotes-app/src/components/Notes.jsx
@@ -1,9 +1,14 @@
 import "./Notes.css";
 function Notes({ notes }) {
+  console.log(notes);
+
   return (
     <div className="notes">
-      {notes.map((note) => (
-        <div className="note">{note}</div>
+      {notes.map((note, index) => (
+        <div key={index} className="note">
+          <div className="date">{note.date}</div>
+          <div className="text">{note.text}</div>
+        </div>
       ))}
     </div>
   );
