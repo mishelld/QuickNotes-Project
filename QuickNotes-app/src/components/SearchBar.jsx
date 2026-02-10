b9fd7208dbf9cb4bcbda2ed3829be3293be6e0c5
mishelld <mishelldub@gmail.com>
2026-02-10 21:34:19 +0200

    Add css to Search Bar

--- QuickNotes-app/src/components/SearchBar.jsx
+++ QuickNotes-app/src/components/SearchBar.jsx
@@ -1,12 +1,15 @@
+import "./SearchBar.css";
 function SearchBar({ search, setSearch }) {
   return (
-    <input
-      value={search}
-      onChange={(e) => {
-        setSearch(e.target.value);
-      }}
-      placeholder="search notes..."
-    ></input>
+    <div className="search-bar">
+      <input
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+        placeholder="search notes..."
+      ></input>
+    </div>
   );
 }
 export default SearchBar;
